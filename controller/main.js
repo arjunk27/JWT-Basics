33459bbfc43ddf74c7c2d40f6a3c5b499070f7bd
“arjunk” <arjunk2079@gmail.com>
2022-05-22 22:26:21 +0530

    jwt demo

--- controller/main.js
+++ controller/main.js
@@ -1,7 +1,13 @@
+const jwt = require("jsonwebtoken");
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) res.status(400).send("Incomplete request");
-  res.send("t");
+  //just for demo,, normaly provided by DB !!!!
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user Created", token });
 };
 
 const dashboard = async (req, res) => {
